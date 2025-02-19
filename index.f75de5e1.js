3376252ec705551b38a07ee829c3e616c90c04e1
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-19 15:36:36 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+'use strict';
+const inputs = Array.from(document.querySelectorAll('input'));
+for (const input of inputs){
+    if (!input.name) input.name = 'input name is empty';
+    if (input.getAttribute('id') === null) input.id = 'identification code' + Math.random().toString(36).substr(2, 9);
+    const textOfPlaceholder = input.name;
+    input.setAttribute('placeholder', capitalizeText(textOfPlaceholder));
+    const newLabel = document.createElement('label');
+    newLabel.setAttribute('class', 'field-label');
+    newLabel.setAttribute('for', input.id);
+    newLabel.textContent = textOfPlaceholder.toUpperCase();
+    input.before(newLabel);
+}
+function capitalizeText(text) {
+    return text[0].toUpperCase() + text.slice(1);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
